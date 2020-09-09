01f2357491304b00b43224d5cc2e04da55fb81e4
baobao-li <a1261964921@qq.com>
2020-09-09 17:57:00 +0800

    2020/9/9

--- blog/src/.vuepress/config.js
+++ blog/src/.vuepress/config.js
@@ -1,11 +1,13 @@
 module.exports = {
   dest: './docs/',
   base: '/Blog/',
-  title: "知识点总结",
-  description: '任何不能够给你快乐的编程都是耍流氓',
-  // markdown: {
-  //   toc: { includeLevel: [2, 3] }
-  // },
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: '前端知识点总结',
+      description: '任何不能够给你快乐的编程都是耍流氓'
+    }
+  },
   head: [
     ['link', { rel: 'icon', href: '/logo.jpg' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
@@ -13,30 +15,89 @@ module.exports = {
   // 主题
   theme: 'reco',
   themeConfig: {
-    // 博客配置
-    // blogConfig: {
-      // category: {
-        // location: 2,     // 在导航栏菜单中所占的位置，默认2
-        // text: 'Category' // 默认文案 “分类”
-      // },
-    // },
     // 搜索设置
     search: true,
     searchMaxSuggestions: 10,
     // 导航栏 Logo
     logo: '/logo.jpg',
     // 开启侧边栏
-    sidebar: 'auto',
-    sidebarDepth: 1,
-    // 导航
-    nav: [
-      {text: '首页',link: '/',icon: 'reco-home'},
-      {text: '快速搭建开发环境',link: '/exploit/Start.md',icon: 'reco-document'},
-      {text: 'Web前端',link: '/web',items: [
-        { text: 'Vue', link: '/web/Vue.md' },
-        { text: 'React', link: '/web/React.md' },]},
-      {text: 'GitHub',link: 'https://github.com/Baobao-Li/Blog',icon: 'reco-github'},
-    ],
+    // sidebar: 'auto',
+    // sidebarDepth: 1,
+    locales: {
+
+      '/': {
+        label: '简体中文',
+        repo: 'baobao-li/Blog',
+        selectText: '选择语言',
+        editLinkText: '帮助我们改进内容',
+        lastUpdated: '上次更新',
+        // 导航
+        nav: [
+          { text: '首页', link: '/', icon: 'reco-home' },
+          { text: '快速搭建开发环境', link: '/exploit/', icon: 'reco-document' },
+          {
+            text: 'Web知识点笔记',
+            items: [
+              { text: '思维导图', link: '/web/Mind/' },
+              { text: 'Vue', link: '/web/Vue/' },
+              { text: 'React', link: '/web/React/' },
+              { text: 'React Hooks', link: '/web/ReactHooks/' },
+              { text: '小程序', link: '/web/VX/' },
+              { text: 'Webpack', link: '/web/Webpack/' },
+              { text: 'TypeScript', link: '/web/Ts/' },
+              { text: 'Git', link: '/web/Git/' },
+            ]
+          },
+          { text: 'GitHub', link: 'https://github.com/Baobao-Li/Blog', icon: 'reco-github' },
+        ],
+        // 侧边栏
+        sidebar: {
+          '/exploit/': [{
+            title: '快速搭建开发环境', children: [
+              ''
+            ]
+          }],
+          '/web/Mind/': [{
+            title: '思维导图版知识点总结', children: [
+              ''
+            ]
+          }],
+          '/web/Vue/': [{
+            title: 'Vue知识点', children: ['', 'Es6', 'Es6方法扩展', 'Webpack', 'Babel', 'Promise', 'Vue 各种方法之间的区别', 'Vue组件', 'Vue路由', 'Vue动画', 'vue钩子函数', 'vuex']
+          }],
+          '/web/React/': [{
+            title: 'React知识点', children: [
+              '', 'JSX', 'MVVM', 'Props', 'React生命周期', 'React组件', 'React路由', 'redux', 'redux中间件', '组件通信', '高阶组件'
+            ]
+          }],
+          '/web/ReactHooks/': [{
+            title: 'React Hooks', children: [
+              ''
+            ]
+          }],
+          '/web/VX/': [{
+            title: '小程序知识点', children: [
+              '', 'WXS', 'wepy组件通信', '小程序事件', '小程序路由', '小程序组件', '小程序组件化'
+            ]
+          }],
+          '/web/Webpack/': [{
+            title: 'Webpack', children: [
+              '',
+            ]
+          }],
+          '/web/Ts/': [{
+            title: 'TypeScript', children: [
+              'TypeScript基础用法','TypeScript高级用法']
+          }],
+          '/web/Git/': [{
+            title: 'Git操作', children: [
+              '']
+          }],
+        },
+      }
+    },
+
+
 
     // 最后更新时间
     lastUpdated: true,
---
+++ blog/src/web/Git/README.md
@@ -0,0 +1,27 @@
+# Git操作 
+
+| 代码                                             | 注释                     |
+| ------------------------------------------------ | ------------------------ |
+| git clone SSH地址                                | 克隆远程仓库代码         |
+| git add .                                        | 暂存                     |
+| git pull                                         | 拉取远程代码             |
+| git push                                         | 提交给本地仓库           |
+| git merge dev                                    | 合并分支代码             |
+| git branch -d 分支名                             | 删除本地分支             |
+| git push origin --delete 分支名                  | 删除远程分支             |
+| git branch                                       | 查看本地分支             |
+| git branch -a                                    | 查看本地和远程的所有分支 |
+| git checkout -b dev                              | 创建分支 并切换分支      |
+| git checkout  dev                                | 切换分支                 |
+| git init                                         | 初始化本地仓库           |
+| git status                                       | 查看代码修改情况         |
+|                                                  | nothing                  |
+|                                                  | modified                 |
+|                                                  | new file                 |
+|                                                  |                          |
+| git reset --hard HEAD^                           | 回退上一个版本           |
+| git reset --hard sassaffd                        | 回到记录的地方           |
+| git reflog                                       | 查看所有时间点的日志     |
+| git log                                          | 查看当前时间点之前日志   |
+| git log --graph                                  | 图形化界面日志           |
+| git log --graph --pretty=oneline --abbrev-commit | 简化版日志               |
